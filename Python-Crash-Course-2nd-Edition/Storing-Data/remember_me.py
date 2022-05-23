47e39ab2b72ca72ebd6da436723a8fe05194a3b2
thom <hxy449999@gmail.com>
2022-05-23 12:02:30 +0800

    remember_me.py updated: move all the greet related code into a function.

--- Python-Crash-Course-2nd-Edition/Storing-Data/remember_me.py
+++ Python-Crash-Course-2nd-Edition/Storing-Data/remember_me.py
@@ -6,13 +6,19 @@ prompt for the username and store it.
 
 filename = 'username.json'
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
-    username = input("What is your name? ")
-    with open(filename, 'w') as f:
-        json.dump(username, f)
-        print(f"We'll remember you when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
+
+def greet_user():
+    """Greet the user by name."""
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        print(f"Welcome back, {username}!")
+
+
+greet_user()
